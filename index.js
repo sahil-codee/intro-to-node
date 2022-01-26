087fc7b939f766640adebb519191a84430b9fdf9
Your Name <you@example.com>
2022-01-26 22:34:32 +0530

    create superheroes and villains

---
+++ index.js
@@ -0,0 +1,13 @@
+// const superheroes = require('superheroes');
+ 
+// var mysuperhero = superheroes.random();
+// //=> 'Spider-Ham'
+
+// console.log(mysuperhero);
+
+const supervillains = require('supervillains');
+ 
+let mysupervillains = supervillains.random();
+//=> 'Mud Pack'
+
+console.log(mysupervillains);
